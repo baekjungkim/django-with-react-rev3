e42dceddc9afe71da1fc39a931fca1fea5cc1c8d
baekjung <zvgandam@gmail.com>
2020-07-31 10:18:25 +0900

    EP02 - JSON 응답뷰 만들기

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+# def post_list(request):
+#     # request.method # => 2개분기
+#     # GET, POST
+#     pass
+
+
+# def post_detail(request, pk):
+#     # request.method # => 3개 분기
+#     # GET, PUT, DELETE
+#     pass
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    # def dispatch(self, request, *args, **kwargs):
+    #     print("request.body :", request.body)  # print 비추, logger cncjs
+    #     print("request.POST :", request.POST)
+    #     return super().dispatch(request, *args, **kwargs)
+
