b9f5dd1cfecd1d785883ac7c92449fde0ea6b356
baekjung <zvgandam@gmail.com>
2020-08-04 13:39:57 +0900

    EP06 - ViewSet과 Router

--- instagram/urls.py
+++ instagram/urls.py
@@ -9,6 +9,6 @@ router.register("post", views.PostViewSet)  # 2개 URL을 만들어줌.
 
 urlpatterns = [
     # path("public/", views.PublicPostListAPIView.as_view()),
-    path("public/", views.public_post_list),
+    # path("public/", views.public_post_list),
     path("", include(router.urls)),
 ]
