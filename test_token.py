543bebd4e36ec95fc87b59bdcd6d744f230c2fd8
baekjung <zvgandam@gmail.com>
2020-08-12 13:17:28 +0900

    EP15 - JWT 인증

--- test_token.py
+++ test_token.py
@@ -3,14 +3,13 @@ import requests  # pip install requests
 # TOKEN = "7dcc37c6bb06a2f4880be7841d909ae6f8c116da"
 JWT_TOKEN = (
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9."
-    "eyJ1c2VyX2lkIjoyLCJ1c2VybmFtZSI6InVzZXIyIiwiZXhwIjoxNTk3MjAyNDUzLCJlbWFpbCI6IiIsIm9yaWdfaWF0IjoxNTk3MjAyMTUzfQ."
-    "UsYEF34b3pZXs4T3_naqg0ZST7laVS-dwm1pRj2TIWM"
+    "eyJ1c2VyX2lkIjoyLCJ1c2VybmFtZSI6InVzZXIyIiwiZXhwIjoxNTk3MjA1ODg5LCJlbWFpbCI6IiIsIm9yaWdfaWF0IjoxNTk3MjA1NTg5fQ."
+    "aeB7uMDhy4M0yZC2Qrosu5zeQI81OuwvUM9DGUNCN1U"
 )
 
 # "eyJ1c2VyX2lkIjoyLCJ1c2VybmFtZSI6InVzZXIyIiwiZXhwIjoxNTk3MjAxNTYzLCJlbWFpbCI6IiIsIm9yaWdfaWF0IjoxNTk3MjAxMjYzfQ" -- payload
 
 headers = {
-    # "Authorization": f"Token {TOKEN}", # Token 인증
     "Authorization": f"JWT {JWT_TOKEN}",  # JWT 인증
 }
 
